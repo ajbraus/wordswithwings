5f1177762bdb47a761996d445758f90060485eeb
sushmit sarmah <sushmit.sarmah@gramener.com>
2015-01-11 11:39:11 +0530

    trying out ace options

--- wordswithwings/public/javascripts/angular_editor/main-controller.js
+++ wordswithwings/public/javascripts/angular_editor/main-controller.js
@@ -10,10 +10,12 @@ angular.module('MainController',[])
         // angular watch function for the input text
         $scope.$watch(
 
+            // set the watch variable to inputText
             function(scope){ 
                 return scope.inputText;
             },
 
+            // set what happens on change of watch variable
             function(data){
                 // get syllables only when a space or end of line has occurred
                 if( [" ","\n"].indexOf(data.slice(-2,-1)) != -1 ){
@@ -35,6 +37,14 @@ angular.module('MainController',[])
             }
         );
 
+        // ace onload function
+        $scope.aceLoaded = function(_editor) {
+            // Options
+            var _session = _editor.getSession();
+            var _renderer = _editor.renderer;
+        };
+
+        // define function to syllables
         function getSyllables(words, callback){
             // function to get syllables from server with parameters as
             // unique words from input text
